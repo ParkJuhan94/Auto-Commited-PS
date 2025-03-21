52e0f3e8a83c52e30a0928a37333c9f558febe95
박주한 <1004sunye@naver.com>
2025-03-22 01:15:25 +0900

    [level 1] Title: 체육복, Time: 0.29 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 68 MB, 시간: 3.50 ms
+메모리: 85.6 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 21:51:34
+2025년 03월 22일 01:15:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,36 +1,42 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 여벌 체육복을 가진 학생이 도난당했을 경우를 처리하기 위해 배열을 복사
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        // List로 변환
+        List<Integer> lostList = new ArrayList<>();
+        for (int l : lost) {
+            lostList.add(l);
+        }
+        List<Integer> reserveList = new ArrayList<>();
+        for (int r : reserve) {
+            reserveList.add(r);
+        }
         
-        // 여벌 체육복을 가진 학생 중 도난당한 학생을 먼저 제외
-        for (int i = 0; i < reserve.length; i++) {
-            for (int j = 0; j < lost.length; j++) {
-                if (reserve[i] == lost[j]) {
-                    reserve[i] = -1; // 여벌 체육복을 가진 학생 표시
-                    lost[j] = -1;   // 도난당한 학생 표시
-                    break;
-                }
+        // lost와 reserve에 동시에 있는 학생은 제거 (자기 체육복만 가진다고 처리)
+        List<Integer> intersection = new ArrayList<>(lostList);
+        intersection.retainAll(reserveList);
+        lostList.removeAll(intersection);
+        reserveList.removeAll(intersection);
+        
+        // 정렬
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+        
+        // 체육 수업 참여 가능 학생 수 (초기에는 전체 학생수에서 lost 리스트에 남은 학생 수를 뺀 값)
+        int answer = n - lostList.size();
+        
+        // 그리디 방식으로 빌려주기
+        for (int lostStudent : lostList) {
+            // 앞번호 학생부터 확인 (더 작은 번호부터 처리하는 것이 그리디 방식)
+            if (reserveList.contains(lostStudent - 1)) {
+                answer++;
+                reserveList.remove(Integer.valueOf(lostStudent - 1));
+            } else if (reserveList.contains(lostStudent + 1)) {
+                answer++;
+                reserveList.remove(Integer.valueOf(lostStudent + 1));
             }
         }
         
-        // 체육복 빌려주는 과정을 처리
-        for (int i = 0; i < reserve.length; i++) {
-            for (int j = 0; j < lost.length; j++) {
-                if (reserve[i] != -1 && lost[j] != -1 && 
-                   (reserve[i] == lost[j] - 1 || reserve[i] == lost[j] + 1)) {
-                    reserve[i] = -1; // 여벌 체육복을 빌려줌
-                    lost[j] = -1;   // 도난당한 학생 해결
-                    break;
-                }
-            }
-        }
-        
-        // 체육복을 가진 학생 수 계산
-        int answer = n - (int) Arrays.stream(lost).filter(x -> x != -1).count();
         return answer;
     }
 }
