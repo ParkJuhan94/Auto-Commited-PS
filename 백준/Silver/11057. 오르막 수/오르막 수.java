74c2c7ecf2a5062a2f705712090d1dc21347e0d2
박주한 <1004sunye@naver.com>
2025-07-27 07:29:01 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -2,38 +2,38 @@
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
-    static int N, M;
+    static final int MOD = 10007;
 
     public static void main(String[] args) throws IOException {
-     //   System.setIn(new FileInputStream("src/BOJ/Section11/P11057/input.txt"));
-
+   //     System.setIn(new FileInputStream("src/BOJ/Section11/P11057/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[10][N + 1];
-        for(int i = 0; i < 10; i++) {
-            dp[i][1] = 1;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[N + 1][10]; // dp[i][j] = 길이가 i이고 마지막 숫자가 j인 오르막 수의 개수
+
+        // 초기값: 길이가 1일 때는 각 숫자 하나씩
+        for (int j = 0; j <= 9; j++) {
+            dp[1][j] = 1;
         }
 
-        for(int i = 2; i <= N; i++) {
-            for(int j = 0; j < 10; j++) {
-                for(int k = 0; k <= j; k++) {
-                    dp[j][i] += dp[k][i - 1];
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
                 }
-                dp[j][i] %= 10007;
             }
         }
 
-        int answer = 0;
-        for(int i = 0; i < 10; i++) {
-            answer += dp[i][N];
+        // 길이 N이고 마지막 숫자가 0~9인 모든 경우 합
+        int result = 0;
+        for (int j = 0; j <= 9; j++) {
+            result = (result + dp[N][j]) % MOD;
         }
 
-        System.out.println(answer % 10007);
+        System.out.println(result);
     }
-
 }
